b46c5e66d2e3e177c30be47beadd4caaf0e01757
PremkumarDivakaran <dprem1006@gmail.com>
2023-09-02 15:53:52 +0530

    first commit from branch

---
+++ src/main/java/com/leaftaps/testcases/TC002_NewBranch.java
@@ -0,0 +1,24 @@
+package com.leaftaps.testcases;
+
+import com.framework.testng.api.base.ProjectHooks;
+import org.testng.annotations.BeforeTest;
+
+public class TC002_NewBranch extends ProjectHooks{
+	@BeforeTest
+	public void setValues() {
+		System.out.println("hello33");
+		System.out.println("testing'");
+		System.out.println("testinfg 3");
+		System.out.println("hello33");
+		System.out.println("testing'");
+		System.out.println("hello");
+		System.out.println("testin");
+		System.out.println("testing4");
+		System.out.println("testing5");
+	}
+	
+	
+
+	
+
+}
